5207cd542b6fa4e8c0a3f469b2eeafe7e7f5ef28
pedro puppin <puppin.pedro@gmail.com>
2023-04-12 11:59:10 -0300

    directives cheat

--- Semana4_angular/src/app/shared/directives/red.directive.ts
+++ Semana4_angular/src/app/shared/directives/red.directive.ts
@@ -5,6 +5,10 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class RedDirective {
   constructor(elementRef: ElementRef) { //recebe uma ref de um elemento HTML quando for utilizada
+    // faz a manipulação do comportamento
     elementRef.nativeElement.style.color = 'blue';
   }
 }
+
+// As diretivas de atributo tratam de alterar a aparência e o comportamento do elemento dom
+// cria a pasta de diretivas dentro da pasta que vai estar a alteração -> comando "ng g directive nome-dir" ou "ng g d nome-dir"
