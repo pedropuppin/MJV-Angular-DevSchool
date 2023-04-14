ab13e84325f81fefedb738f5b395bc689054fb2c
pedro puppin <puppin.pedro@gmail.com>
2023-04-13 21:33:26 -0300

    rxjs part1

--- RxJS_introducao/index.js
+++ RxJS_introducao/index.js
@@ -56,31 +56,88 @@ const users = {
   ],
 };
 
+const users2 = {
+  data: [
+    {
+      id: 1,
+      status: "active",
+      age: 14,
+    },
+    {
+      id: 2,
+      status: "inactive",
+      age: 12,
+    },
+    {
+      id: 3,
+      status: "active",
+      age: 42,
+    },
+    {
+      id: 4,
+      status: "inactive",
+      age: 42,
+    },
+    {
+      id: 5,
+      status: "active",
+      age: 13,
+    },
+    {
+      id: 6,
+      status: "inactive",
+      age: 75,
+    },
+    {
+      id: 7,
+      status: "inactive",
+      age: 43,
+    },
+    {
+      id: 8,
+      status: "inactive",
+      age: 54,
+    },
+    {
+      id: 9,
+      status: "active",
+      age: 7,
+    },
+    {
+      id: 10,
+      status: "active",
+      age: 17,
+    },
+  ],
+};
+
+
 
 // OBSERVABLE --> o Observable fornece os dados. Pede fornecer multipos dados diferentes
 
 const observable = new Observable((subscriber) => { // instancia uma nova classe Observable que recebe um parametro "subscriber"
+  subscriber.next(users2);
   subscriber.next(users);
-}).pipe(
+}).pipe( // .pipe faz com que os dado passem pelos OPERATORS dentro do pipe antes de irem para o oserver
   map((value) => {
-    console.log('1 - recebeu dados do observable', value);
+    // console.log('1 - recebeu dados do observable', value);
     return value.data
   }),
   map((value) => {
-    console.log('2 - recebeu dados do primeiro operator', value);
+    // console.log('2 - recebeu dados do primeiro operator', value);
     return value.filter(users => users.status === "active")
   }),
   map((value) => {
-    console.log('3 - recebeu dados do segundo operator', value);
+    // console.log('3 - recebeu dados do segundo operator', value);
     return value.reduce((sum, user) => sum + user.age, 0) / value.length;
   }),
   map((value) => {
-    console.log('4 - recebeu dados do terceiro operator', value);
+    // console.log('4 - recebeu dados do terceiro operator', value);
     if (value < 18)
       throw new Error('Average age is to young');
     else return value
   })
-); // .pipe faz com que os dado passem pelos OPERATORS dentro do pipe
+);
 
 // OPERATORS --> é interessante limitar o que cada operator faz a uma única tarefa, por exemplo, o primeiro map é apenas responsável
 // por extrair a array "data" de dentro do users e passar para o próximo operator. Sempre o último operator do pipe que manda
